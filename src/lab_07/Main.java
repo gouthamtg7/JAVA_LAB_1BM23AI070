839a5c5b071b87903d1a6dad4eac50ad27c3c516
gouthamtg7 <gouthamtaranagar.ai23@bmsce.ac.in>
2025-05-27 06:41:06 +0530

    Pharma

---
+++ src/lab_07/Main.java
@@ -0,0 +1,4 @@
+package lab_07;
+
+public class Main {
+}
---
+++ src/lab_07/program_1_Pharmacy.java
@@ -0,0 +1,11 @@
+package lab_07;
+
+import java.util.ArrayList;
+class Pharmacy {
+    public final int id;
+    public final String name;
+    
+    public Pharmacy(){
+
+    }
+}
